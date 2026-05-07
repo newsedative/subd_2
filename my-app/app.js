6a5203a3c0010d6a2d019b32589d0e97e1df49fb
Dsh <knowslana@gmail.com>
2026-05-07 22:05:02 +0300

    mongodb

--- my-app/app.js
+++ my-app/app.js
@@ -4,11 +4,29 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const pgp = require('pg-promise')(/* options */)
-const db = pgp('postgres://postgres:admin@localhost:5432/lab_2')
+const pgp = require('pg-promise')(/* options */);
+const db = pgp('postgres://postgres:admin@localhost:5432/lab_2');
 
 const { connectRedis } = require('./redis');
 const cache = connectRedis();
+
+const { MongoClient } = require('mongodb');
+const url = 'mongodb://localhost:27017'; 
+const client = new MongoClient(url);
+const dbName = 'lab_3'; 
+
+let mongoDb; 
+
+async function startMongo() {
+    try {
+        await client.connect();
+        console.log("✅ MongoDB успешно подключена");
+        mongoDb = client.db(dbName);
+    } catch (err) {
+        console.error("❌ Ошибка подключения к MongoDB:", err);
+    }
+}
+startMongo();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,9 +36,8 @@ var paymentsRouter = require('./routes/payments');
 
 var app = express();
 
-session = require("./session.js")
+session = require("./session.js");
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -30,11 +47,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req,res,next){
-    req.db = db;
-    req.cache = cache;
+app.use(function(req, res, next) {
+    req.db = db;           
+    req.cache = cache;     
+    
+    if (mongoDb) {
+        req.mongo = mongoDb; 
+    }
     next();
-})
+});
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -44,24 +66,21 @@ app.use('/payments', paymentsRouter);
 
 var api      = require('./routes/api');
 app.use('/api', api);
+
 var api_auth = require('./routes/api/auth');
 api.use('/auth', api_auth);
 
 var api_users = require('./routes/api/users');
 api.use('/users', api_users);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
--- my-app/routes/users.js
+++ my-app/routes/users.js
@@ -16,5 +16,18 @@ router.get('/', async function(req, res, next) {
 
 });
 
+// Пример получения доп. данных из коллекции пользователей Mongo
+router.get('/:id/extra', async function(req, res) {
+    const userId = req.params.id;
+    
+    // Обращаемся к коллекции 'users' в MongoDB [cite: 20]
+    const collection = req.mongo.collection('users');
+    
+    // Ищем документ (в Mongo id часто хранится как число или ObjectId)
+    const extraInfo = await collection.findOne({ postgres_id: parseInt(userId) });
+    
+    res.json(extraInfo || { message: "Дополнительная информация не найдена" });
+});
+
 
 module.exports = router;
